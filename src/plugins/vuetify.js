a363e8610714fe646b0f89e126b4a2f1169c02ae
mpargar <miguel.parra0542@gmail.com>
2019-09-12 15:17:11 -0600

    Theme and load

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,6 +4,15 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    primary: '#1976D2',
+    secondary: '#EFEFEF',
+    accent: '#FF5252',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  },
   icons: {
     iconfont: 'mdi',
   },
