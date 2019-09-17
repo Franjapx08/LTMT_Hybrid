68fb9264f8f2432cbc3bd0b925faecc1f6e4f62b
mpargar <miguel.parra0542@gmail.com>
2019-09-17 01:33:54 -0600

    Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="login-wrapper">
+    <!-- BAR -->
     <v-app-bar color="primary" app>
       <img id="logo" src="../assets/logo.png"/>
       <span class="title">Iniciar Sesión</span>
     </v-app-bar>
+    <!-- BODY -->
     <v-content class="contenido">
       <v-card class="login-container">
         <v-text-field
@@ -25,6 +27,26 @@
         <v-btn @click="login()" :disabled="disableInputs" :loading="disableInputs" color="primary">Login</v-btn>
       </v-card>
     </v-content>
+    <!-- Messages -->
+    <v-dialog
+      v-model="showDialog"
+    >
+      <v-card>
+         <v-card-title class="headline">Error</v-card-title>
+         <v-card-text>
+          {{ message }}
+        </v-card-text>
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+          <v-btn
+            color="primary"
+            text
+            @click="showDialog = false">
+            Ok
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -33,12 +55,16 @@ import { login } from '../utils/services'
 export default {
   name: 'App',
   data: () => ({
+    /* FORM */
     email: '',
     password: '',
     showPassword: false,
     disableInputs: false,
     uuid: null,
-    model: null
+    model: null,
+    /* DIALOG */
+    message: '',
+    showDialog: ''
   }),
   created () {
     // eslint-disable-next-line
@@ -56,7 +82,12 @@ export default {
       data.append('model', this.model)
       login(data).then(response => {
         // eslint-disable-next-line
-        console.log(response)
+        if (response.data.code === 1) {
+
+        } else {
+          this.message = response.data.message
+          this.showDialog = true
+        }
         this.disableInputs = false
       })
     }
